e23a62fbd9ce635e309a9276e0f7a085ac844310
Benjamin Gwin <gwin@ualberta.ca>
2012-01-25 22:32:58 -0700

    Added scrolling message functionality

--- InternetSign/InternetSign.c
+++ InternetSign/InternetSign.c
@@ -6,6 +6,8 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <string.h>
 #include "Font.h"
 
 #define F_CPU 1000000UL
@@ -38,6 +40,11 @@
 
 #define ADR_SIZE 7
 #define DATA_SIZE 4
+
+#define MSG_LENGTH 256
+#define CHARH 7
+#define CHARW 5
+#define SIGNW 4 * 8
 
 void delay() {
     for (int i = 0; i < 100; i++);
@@ -114,7 +121,7 @@ void write_char(char c, uint8_t col) {
     write_data_msb(WR_PREFIX, PREFIX_SIZE);
     write_data_msb(addr, ADR_SIZE);
     for (int i = 0; i < 5; i++) {
-        data = Font5x7[(c - 32) * 5 + i];
+        data = pgm_read_byte(Font5x7 + (c - 32) * 5 + i);
         write_data_lsb(data, DATA_SIZE);
         write_data_lsb(data >> 4, DATA_SIZE);
     }
@@ -131,8 +138,52 @@ void clear_display() {
     write_end();
 }
 
+static char message[MSG_LENGTH] = "THIS THING IS REALLY FLAKY";
+static uint8_t frame_buffer[SIGNW];
+
+void set_buffer(uint8_t* buffer, uint8_t data, uint8_t idx) {
+	if (idx < 0 || idx > SIGNW) {
+		return;
+	}
+	buffer[idx] = data; 
+} 
+
+void update_buffer(const char* msg, uint8_t* buffer, int idx) {
+	int real_pos = 0;
+	
+	memset((void *)buffer, 0, SIGNW);
+	
+	for (int i = 0; i < strlen(msg); i++) {
+		char c = msg[i];
+		if (c == ' ') {
+			real_pos += 2;
+			set_buffer(buffer, 0, real_pos + idx);
+			set_buffer(buffer, 0, real_pos + idx + 1);
+		} else {
+			for (int j = 0; j < 5; j++) {
+				real_pos++;
+                uint8_t data = pgm_read_byte(Font5x7 + (c - 32) * 5 + j);
+                set_buffer(buffer, data, real_pos + idx);
+            }
+			real_pos += 1;
+		    set_buffer(buffer, 0, real_pos + idx);
+		}
+	}
+}
+
+void write_buffer(const uint8_t* buffer) {
+	write_begin();
+	write_data_msb(WR_PREFIX, PREFIX_SIZE);
+	write_data_msb(0, ADR_SIZE);
+	for (int i = 0; i < SIGNW; i++) {
+		write_data_lsb(buffer[i], DATA_SIZE);
+		write_data_lsb(buffer[i] >> 4, DATA_SIZE);
+	}
+	write_end();
+}
+
 int main(void)
-{
+{	
     set_output(DDRD, CLK);
     set_output(DDRD, LED);
     set_output(DDRD, DATA);
@@ -146,27 +197,23 @@ int main(void)
     write_command(COM_OPTION);
     write_command(PWM_DUTY_8);
     
-    clear_display();
-    
-    int idx = 0;
-    int direction = 1;
-    while(1)
-    {
-        clear_display();
-        write_char('E', idx);
-        write_char('E', 5 + idx);
-        write_char('4', 10 + idx);
-        write_char('0', 15 + idx);
-        write_char('1', 20 + idx);
-        
-        idx += direction;
-        if (idx > 8) {
-            direction = -1;
-            idx = 8;
-        } else if (idx == 0) {
-            direction = 1;
-            idx = 0;
-        }                
-        _delay_ms(100);            
-    }
+	int length = 0;
+	for (int i = 0; i < strlen(message); i++) {
+		if (message[i] == ' ') {
+			length += 2;
+		} else {
+			length += CHARW + 1;
+		}
+	}
+	
+	uint8_t i = SIGNW;
+	while (1) {
+		update_buffer(message, frame_buffer, i);
+		i--;
+		if (i < 0) {
+			i = SIGNW;
+		}			
+	    write_buffer(frame_buffer);
+		_delay_ms(20);
+	}
 }
